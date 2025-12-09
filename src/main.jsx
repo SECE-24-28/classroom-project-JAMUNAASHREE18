e773300bbbff960681b2bf6712001f0b9187b9b4
JAMUNAASHREE18 <jamunaashree18@gmail.com>
2025-12-09 20:31:35 +0530

    7th day practice

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './App.jsx'
-
+import Display2 from './Display2.jsx'
+import Display from './useeffect.jsx'
+import Display5 from './display5.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Display2 />
+    <Display5 />
         {/* <Display/> */}
+
   </StrictMode>,
 )
